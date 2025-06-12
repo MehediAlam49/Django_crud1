933784cab8f43f88d9686c733ba8ee4d45775c78
MehediAlam49 <mehedialam806@gmail.com>
2025-06-12 08:37:30 +0600

    complete home page and mastering

--- crud1/crud1/urls.py
+++ crud1/crud1/urls.py
@@ -6,4 +6,9 @@ from crud1.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('add-product', addProduct, name='addProduct'),
+    path('product-List', productList, name='productList'),
+    path('edit-product', editProduct, name='editProduct'),
+    path('delete-product', deleteProduct, name='deleteProduct'),
+    path('view-product', viewProduct, name='viewProduct'),
 ]
--- crud1/crud1/views.py
+++ crud1/crud1/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render,redirect
 
 def home(request):
     return render(request, 'home.html')
+
+def addProduct(request):
+    return render(request, 'addProduct.html')
+
+def productList(request):
+    return render(request, 'productList.html')
+
+def editProduct(request):
+    return render(request, 'editProduct.html')
+
+def deleteProduct(request):
+    return redirect('productList.html')
+
+def viewProduct(request):
+    return render(request, 'viewProduct.html')
